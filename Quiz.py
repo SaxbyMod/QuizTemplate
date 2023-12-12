0013ff47ebeb7a735b6d13908d52b5c722146bb3
Thin Creator 3483 <102002463+SaxbyMod@users.noreply.github.com>
2023-12-12 14:26:34 -0500

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,11 +1,31 @@
+import os
+# Questions
 Q1 = "Is this readable? "
-Awnser = input(Q1 + "True or False?: ")
+
+# Types
+TorF = "True or False?: "
+ABC = "A B or C: "
+ABCD = "A B C or D: "
+ABCDE = "A B C D or E: "
+ShortResponse = "Please write a Short Response to awnser this Question: "
+LongResponse = "Please write a Long form Response to awnser this Question: "
+
+# Run
+Awnser1 = input(Q1 + TorF)
 Score = 0
-if Awnser == "t" or Awnser == "T" or Awnser == "True" or Awnser == "true" or Awnser == "TrUe" or Awnser == "tRuE" or Awnser == "TRUE":
+if Awnser1 == "t" or Awnser1 == "T" or Awnser1 == "True" or Awnser1 == "true" or Awnser1 == "TrUe" or Awnser1 == "tRuE" or Awnser1 == "TRUE":
 	Score = Score+1
 	print("That is correct")
 else:
-	Score = Score-1
 	print("That is incorrect")
 
+# Save
+
 print(Score)
+try:
+    os.mknod('AwnsersForQuiz.txt')
+except FileExistsError as e:
+    print(f'An error occurred: {e}')
+file_object = open(AwnsersForQuiz.txt, w)
+write(Awnser1)
+file_object.close()
