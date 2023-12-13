dc5efb285711ee5a9ed6479fc182789d85a39d1f
Thin Creator 3483 <102002463+SaxbyMod@users.noreply.github.com>
2023-12-12 21:57:07 -0500

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,4 +1,5 @@
 import os
+
 # Questions
 Q1 = "Is this readable? "
 
@@ -7,25 +8,23 @@ TorF = "True or False?: "
 ABC = "A B or C: "
 ABCD = "A B C or D: "
 ABCDE = "A B C D or E: "
-ShortResponse = "Please write a Short Response to awnser this Question: "
-LongResponse = "Please write a Long form Response to awnser this Question: "
+ShortResponse = "Please write a Short Response to answer this Question: "
+LongResponse = "Please write a Long-form Response to answer this Question: "
 
 # Run
-Awnser1 = input(Q1 + TorF)
+Answer1 = input(Q1 + TorF)
 Score = 0
-if Awnser1 == "t" or Awnser1 == "T" or Awnser1 == "True" or Awnser1 == "true" or Awnser1 == "TrUe" or Awnser1 == "tRuE" or Awnser1 == "TRUE":
-	Score = Score+1
-	print("That is correct")
+if Answer1.lower() == "t" or Answer1.lower() == "true":
+    Score += 1
+    print("That is correct")
 else:
-	print("That is incorrect")
+    print("That is incorrect")
 
 # Save
+print("Score:", Score)
 
-print(Score)
 try:
-    os.mknod('AwnsersForQuiz.txt')
-except FileExistsError as e:
+    with open('AnswersForQuiz.txt', 'w') as file_object:
+        file_object.write(f"Answer 1: {Answer1}\n")
+except Exception as e:
     print(f'An error occurred: {e}')
-file_object = open(AwnsersForQuiz.txt, w)
-write(Awnser1)
-file_object.close()
